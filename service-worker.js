dd502ca844b6b8365e55a8e0c83e75569b2a6de0
R@KA <rahitkumaradhekary161369@gmail.com>
2026-04-05 09:41:39 +0600

    Update and rename service-worker.js to sw.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-// service-worker.js
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-    // ক্রোমকে শান্ত রাখার জন্য এই ডামি ফেচ রিকোয়েস্ট
-});
---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "bajus-v5";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./favicon.png",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
