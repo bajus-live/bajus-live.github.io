8b6c3794d75e2b22b1f1084a82b847a293905137
R@KA <rahitkumaradhekary161369@gmail.com>
2026-04-17 21:59:19 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ self.addEventListener("install", e => {
         "./index.html",
         "./style.css",
         "./favicon.png",
-        "./icon_192×192.png",
-        "./icon_512×512.png"
+        "./icon(192×192).png",
+        "./icon(512×512).png"
       ]);
     })
   );
