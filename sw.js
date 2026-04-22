580c00f86c5b5d3bef22aca21cecd51790b5e432
R@KA <rahitkumaradhekary161369@gmail.com>
2026-04-22 06:37:40 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bajus-v1.3";
+const CACHE_NAME = "bajus-v1.4";
 
 self.addEventListener("install", e => {
   e.waitUntil(
@@ -20,18 +20,3 @@ self.addEventListener("fetch", e => {
     caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
-
-
-
-// Firebase Background Messaging
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
-
-firebase.initializeApp({
-  apiKey: "AlzaSyCJm0C-1TCoR3BtkjNEf13avvcO9C11lU8",
-  projectId: "bajus-d2fdd",
-  messagingSenderId: "300016752497",
-  appId: "1:300016752497:web:e8d419a90a9bebc5df48d0"
-});
-
-const messaging = firebase.messaging();
